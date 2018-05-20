a59aeaf68ff0a0fc19cd00d740dd0613775ccad4
HudaSalah <hudasalah29@gmail.com>
2018-05-20 23:29:30 +0200

    header done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AngularFontAwesomeModule } from 'angular-font-awesome';// font awesom
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { CartProductComponent } from './cart-product/cart-product.component';
+import { CartFormComponent } from './cart-form/cart-form.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    CartProductComponent,
+    CartFormComponent
   ],
   imports: [
     BrowserModule,
